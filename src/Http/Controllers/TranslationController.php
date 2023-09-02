6ecba0019aff44ba3f259664deec2dc10dcd4615
Ryan Mitchell <ryan@thoughtcollective.com>
2023-09-02 18:32:17 +0100

    Better title when editing

--- src/Http/Controllers/TranslationController.php
+++ src/Http/Controllers/TranslationController.php
@@ -25,7 +25,7 @@ class TranslationController extends Controller
     public function edit(Request $request, string $locale)
     {
         [$namespaces, $translations] = $this->getDataForLocale($locale);
-            
+
         $blueprint = $this->buildBlueprint($namespaces->toArray());
 
         $fields = $blueprint
@@ -37,7 +37,7 @@ class TranslationController extends Controller
             'blueprint' => $blueprint->toPublishArray(),
             'meta' => $fields->meta(),
             'route' => cp_route('translation-manager.update', $locale),
-            'title' => __('Translation Manager'),
+            'title' => __('Manage Translation: :locale', ['locale' => $locale]),
             'values' => $fields->values(),
         ]);
     }
@@ -45,46 +45,46 @@ class TranslationController extends Controller
     public function update(Request $request, string $locale)
     {
         [$namespaces, $translations] = $this->getDataForLocale($locale);
-            
+
         $blueprint = $this->buildBlueprint($namespaces->toArray());
 
         $fields = $blueprint->fields()->addValues($request->all());
 
         $fields->validate();
-        
+
         $translations = $fields->process()->values()->toArray();
-        
+
         TranslationManager::saveTranslations($locale, $translations);
     }
 
     private function buildBlueprint(array $namespaces): Blueprint
     {
         $path = Path::assemble(__DIR__.'/../../../', 'resources', 'blueprints', 'manager.yaml');
-        
+
         $yaml = YAML::file($path)->parse();
-                                
+
         $tab = $yaml['tabs']['main'];
         unset($yaml['tabs']['main']);
-        
+
         foreach ($namespaces as $namespace) {
             $newTab = $tab;
             Arr::set($newTab, 'display', $namespace);
             Arr::set($newTab, 'sections.0.fields.0.handle', $namespace);
             $yaml['tabs']['tab_'.$namespace] = $newTab;
         }
-        
+
         return BlueprintAPI::make()->setContents($yaml);
     }
-    
+
     private function getDataForLocale($locale)
     {
         $translations = Models\Translation::where('locale', $locale)
             ->orderBy('key')
             ->get()
             ->groupBy('file');
-        
+
         $namespaces = $translations->keys()->sort();
-        
+
         return [$namespaces, $translations];
     }
 }
