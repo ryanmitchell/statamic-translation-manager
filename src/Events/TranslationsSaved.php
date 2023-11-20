8c68daa4afde3701693975e06f537be113bd8c62
Kevin Meijer <kevin@justbetter.nl>
2023-11-20 16:03:58 +0100

    Added commit message to TranslationSaved event

--- src/Events/TranslationsSaved.php
+++ src/Events/TranslationsSaved.php
@@ -4,12 +4,18 @@ namespace RyanMitchell\StatamicTranslationManager\Events;
 
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Statamic\Contracts\Git\ProvidesCommitMessage;
 
-class TranslationsSaved
+class TranslationsSaved implements ProvidesCommitMessage
 {
     use Dispatchable, SerializesModels;
 
     public function __construct(public string $locale, public string $namespace, public array $translations)
     {
+    }
+
+    public function commitMessage()
+    {
+        return __('Translation saved', [], config('statamic.git.locale'));
     }
 }
