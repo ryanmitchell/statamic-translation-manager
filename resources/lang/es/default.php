199608a3a012edca31fc1ede833f047a63789b68
Guillermo Azurdia <guillermo@nopticon.com>
2025-03-26 01:11:19 -0500

    Spanish lang (#30)
    * Spanish lang
    
    * Spanish lang

---
+++ resources/lang/es/default.php
@@ -0,0 +1,19 @@
+<?php
+
+  return [
+    "add_to_pack" => "Agregar al paquete de idiomas",
+    "blueprint_scan" => "Escanear planos",
+    "check_for_missing_translations" => "Comprueba si faltan traducciones",
+    "manage_translations" => "Administrar traducciones",
+    "manage_translation_locale" => "Administrar traducción: :locale",
+    "missing_translations" => ":count traducción faltante|:count traducciones faltantes",
+    "no_missing_translations" => "¡No faltan traducciones! ¡Estás muy bien!",
+    "scan_blueprints" => "Escanear planos",
+    "scan_blueprints_desc" => "Escanea tus planos en busca de cadenas faltantes y agrégalos a tus paquetes de idiomas",
+    "scan_templates" => "Escanear plantillas",
+    "scan_templates_desc" => "Escanea tus plantillas en busca de cadenas faltantes en etiquetas { trans } y agrégalas a tus paquetes de idiomas",
+    "template_scan" => "Escaneo de plantillas",
+    "translations" => "Traducciones",
+    "translation_manager" => "Administrador de traducciones",
+    "translations_added" => "Traducciones agregadas. Busca cadenas en blanco"
+];
